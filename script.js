9b58d5a1f8cb4244c2c078a24d458d29eee68fb8
Kiattisak Dujda <kiattisak.dujda@gmail.com>
2020-02-27 13:53:40 +0000

    Initail commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const hbMenu = document.querySelector("#menu");
+const navList = document.querySelector("#nav");
+
+hbMenu.addEventListener("click", function() {
+  menu.classList.toggle("change");
+  navList.classList.toggle("change");
+});
